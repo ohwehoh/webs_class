d413d6dea9da33d4d2f3b5e7accdc7c0b194b4e2
ohwehoh <ohwehoh@naver.com>
2022-01-20 12:11:55 +0900

    22.01.20.

--- javascript/assets/js/script.js
+++ javascript/assets/js/script.js
@@ -1,7 +1,7 @@
 //  결과보기 버튼을 클릭하면 경고창을 띄어주세요.
 document.querySelectorAll(".result").forEach(function(el){
     el.addEventListener("click", function(){
-        el.querySelector(".result div").classList.toggle("active");
+        el.querySelector(".result > div").classList.toggle("active");
     })
 });
 
