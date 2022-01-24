b73360335785d1a23340f4c4e80c65bc2c697cf2
ohwehoh <ohwehoh@naver.com>
2022-01-24 09:01:51 +0900

    22.01.24.

--- javascript/assets/js/script.js
+++ javascript/assets/js/script.js
@@ -1,6 +1,13 @@
-//  결과보기 버튼을 클릭하면 경고창을 띄어주세요.
+// 결과보기 버튼을 클릭하면 경고창을 띄어주세요.
 document.querySelectorAll(".result").forEach((el)=>{
     el.querySelector("h4").addEventListener("click", ()=>{
-        el.querySelector("div").classList.toggle("active");
+        el.classList.toggle("active");
     })
 });
+
+// document.querySelectorAll(".result").forEach(function(el){
+//     console.log(el);
+//     el.addEventListener("click", function(){
+//         el.classList.toggle("active");
+//     });
+// });
