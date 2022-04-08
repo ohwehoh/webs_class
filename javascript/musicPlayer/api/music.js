e62be6055e8df7ceb0b809995a0b31a8cde199d7
ohwehoh <ohwehoh@naver.com>
2022-04-08 12:18:30 +0900

    22.04.08.

--- javascript/musicPlayer/api/music.js
+++ javascript/musicPlayer/api/music.js
@@ -1,60 +1,60 @@
 let allMusic = [
     {
-        name : "노래 제목 - 제목",
+        name : "노래 제목 - 1",
         artist : "Yeon Woo",
         img : "music-1",
         audio : "music-1"
     },
     {
-        name : "노래 제목 - 제목",
+        name : "노래 제목 - 2",
         artist : "Yeon Woo",
         img : "music-2",
         audio : "music-2"
     },
     {
-        name : "노래 제목 - 제목",
+        name : "노래 제목 - 3",
         artist : "Yeon Woo",
         img : "music-3",
         audio : "music-3"
     },
     {
-        name : "노래 제목 - 제목",
+        name : "노래 제목 - 4",
         artist : "Yeon Woo",
         img : "music-4",
         audio : "music-4"
     },
     {
-        name : "노래 제목 - 제목",
+        name : "노래 제목 - 5",
         artist : "Yeon Woo",
         img : "music-5",
         audio : "music-5"
     },
     {
-        name : "노래 제목 - 제목",
+        name : "노래 제목 - 6",
         artist : "Yeon Woo",
         img : "music-6",
         audio : "music-6"
     },
     {
-        name : "노래 제목 - 제목",
+        name : "노래 제목 - 7",
         artist : "Yeon Woo",
         img : "music-7",
         audio : "music-7"
     },
     {
-        name : "노래 제목 - 제목",
+        name : "노래 제목 - 8",
         artist : "Yeon Woo",
         img : "music-8",
         audio : "music-8"
     },
     {
-        name : "노래 제목 - 제목",
+        name : "노래 제목 - 9",
         artist : "Yeon Woo",
         img : "music-9",
         audio : "music-9"
     },
     {
-        name : "노래 제목 - 제목",
+        name : "노래 제목 - 10",
         artist : "Yeon Woo",
         img : "music-10",
         audio : "music-10"
